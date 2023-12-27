504c09a045906737fb99ed957473d7d640fe62f7
bhc <behicsakar@gmail.com>
2023-12-27 14:45:32 -0500

    Refactor routes to use double quotes and add oauth2 routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
-import { Application } from 'express';
-import homeRoutes from './home.routes';
-import roomRoutes from './room.routes';
-import messageRoutes from './message.routes';
+import { Application } from "express";
+import homeRoutes from "./home.routes";
+import roomRoutes from "./room.routes";
+import messageRoutes from "./message.routes";
+import oauth2Routes from "./oauth2.routes";
 
 export default class Routes {
   constructor(app: Application) {
-    app.use('/api', homeRoutes);
-    app.use('/api/room', roomRoutes);
-    app.use('/api/message', messageRoutes);
+    app.use("/api", homeRoutes);
+    app.use("/api/room", roomRoutes);
+    app.use("/api/message", messageRoutes);
+    app.use("/api/oauth2", oauth2Routes);
   }
 }
