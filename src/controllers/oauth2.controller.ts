c552fa67a13294348cf8d19c6e1355e46ad5a262
bhc <behicsakar@gmail.com>
2023-12-27 15:04:44 -0500

    Remove unnecessary console.log statements

--- src/controllers/oauth2.controller.ts
+++ src/controllers/oauth2.controller.ts
@@ -3,8 +3,6 @@ import { throwIfMissing } from "../utils/utils";
 
 export default class OAuth2Controller {
   async redirect(req: Request, res: Response) {
-    console.log("req: ", req);
-    console.log("req.body: ", req.body);
     console.log("req.headers: ", req.headers);
 
     res.end("processing...");
