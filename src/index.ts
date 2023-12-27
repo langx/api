d957e3030eefa92771e1a7405704c75ebfdd8d62
bhc <behicsakar@gmail.com>
2023-12-27 16:22:03 -0500

    Update CORS origin URL in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express, { Application } from 'express';
-import cors, { CorsOptions } from 'cors';
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
 import Routes from "./routes";
 
 export default class Server {
   constructor(app: Application) {
-    this.config(app);;
+    this.config(app);
     new Routes(app);
   }
 
   private config(app: Application): void {
     // TODO: !IMPORTANT! Change this to your Angular app URL
-    // const corsOptions: CorsOptions = {
-    //   origin: 'http://localhost:8100',
-    // };
-    // app.use(cors(corsOptions));
+    const corsOptions: CorsOptions = {
+      origin: "https://app.languagexchange.net",
+    };
+    app.use(cors(corsOptions));
 
     app.use(cors());
     app.use(express.json());
