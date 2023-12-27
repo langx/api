5ade315f418b3bb59208b0ce2b7b863b77b8d8ef
bhc <behicsakar@gmail.com>
2023-12-27 15:25:44 -0500

    Update OAuth2Controller with axios request

--- src/controllers/oauth2.controller.ts
+++ src/controllers/oauth2.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { Client, Account } from "appwrite";
+// import { Client, Account } from "appwrite";
+import axios from "axios";
 import "dotenv/config";
 
 import { throwIfMissing } from "../utils/utils";
@@ -14,17 +15,35 @@ export default class OAuth2Controller {
   async redirect(req: Request, res: Response) {
     console.log("req.headers: ", req.headers);
 
-    const client = new Client()
-      .setEndpoint(env.APP_ENDPOINT)
-      .setProject(env.APP_PROJECT);
+    // const client = new Client()
+    //   .setEndpoint(env.APP_ENDPOINT)
+    //   .setProject(env.APP_PROJECT);
 
-    const account = new Account(client);
+    // const account = new Account(client);
 
-    const oauth2 = account.createOAuth2Session(
-      "google",
-      "https://api.languagexchnage.net/api/oauth2",
-      "https://app.languagexchnage.net/login/"
-    );
+    // const oauth2 = account.createOAuth2Session(
+    //   "google",
+    //   "https://api.languagexchnage.net/api/oauth2",
+    //   "https://app.languagexchnage.net/login/"
+    // );
+
+    const provider = "google"; // replace with your provider
+
+    axios
+      .get(`/v1/account/sessions/oauth2/${provider}`, {
+        baseURL: env.APP_ENDPOINT,
+        headers: {
+          "Content-Type": "application/json",
+          "X-Appwrite-Response-Format": "1.4.0",
+          "X-Appwrite-Project": env.APP_PROJECT,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
     res.end("processing...");
   }
