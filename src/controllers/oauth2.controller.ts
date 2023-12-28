fb7df0156eecf7916d50e40899b54c899a9397ee
bhc <behicsakar@gmail.com>
2023-12-27 20:14:54 -0500

    Add error handling in OAuth2Controller

--- src/controllers/oauth2.controller.ts
+++ src/controllers/oauth2.controller.ts
@@ -42,8 +42,20 @@ export default class OAuth2Controller {
 
       console.log(response.data);
       res.send(response.data);
-    } catch (error) {
+    } catch (error: any) {
       console.error(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
       res.send(error);
     }
   }
