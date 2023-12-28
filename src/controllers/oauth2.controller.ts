620a778decd61c44da644f12d04d7d49352c8a4f
bhc <behicsakar@gmail.com>
2023-12-27 20:33:44 -0500

    Update headers in OAuth2Controller

--- src/controllers/oauth2.controller.ts
+++ src/controllers/oauth2.controller.ts
@@ -25,7 +25,26 @@ export default class OAuth2Controller {
       hostname: "db.languagexchange.net",
       path: "/v1/account/sessions/oauth2/callback/google/650750d21e4a6a589be3",
       method: "GET",
-      headers: req.headers,
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Referer: "https://app.languagexchange.net/",
+        "Sec-Ch-Ua":
+          '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"macOS"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      },
     };
 
     const agent = new https.Agent({
@@ -55,7 +74,7 @@ export default class OAuth2Controller {
         console.log(error.request);
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
+        console.log("Error", error.message);
       }
       res.send(error);
     }
