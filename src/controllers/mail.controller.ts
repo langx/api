91100f578f8147b27d8972258b67f1ec973de995
bhc <behicsakar@gmail.com>
2024-04-27 22:52:07 +0300

    Add email validation and error handling to MailController

--- src/controllers/mail.controller.ts
+++ src/controllers/mail.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import "dotenv/config";
+import { throwIfMissing } from "../utils/utils";
 
 const env: any = {
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY as string,
@@ -9,6 +10,13 @@ const env: any = {
 export default class MailController {
   async create(req: Request, res: Response) {
     try {
+      throwIfMissing(req.body, ["email"]);
+      // Check if the email is valid
+      const email = req.body.email;
+      if (!email.includes("@")) {
+        return res.status(400).json({ status: "Invalid email" });
+      }
+      // Add the contact to the SendGrid list
       fetch("https://api.sendgrid.com/v3/marketing/contacts", {
         method: "PUT",
         headers: {
@@ -17,10 +25,9 @@ export default class MailController {
         },
         body: JSON.stringify({
           list_ids: [env.NEWSLETTER_LIST_ID],
-          contacts: [{ email: "new_subscriber213@example.com" }],
+          contacts: [{ email }],
         }),
       }).then((response) => {
-        console.log(response);
         if (response.status === 202) {
           console.log("Contact added successfully!");
           return res.json({ status: "ok" });
